e367a3cc6141dc7c880572c928bcf129b2eca5a3
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-20 20:13:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,10 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(username: params["username"])
     if user && user.authenticate(params["password"])
-      session[:id] == 
+      session[:user_id] == user.id 
+      redirect '/account'
+    else 
+      redirect '/failure'
   end
 
   get "/failure" do
